22168540544594a16c931f0254fb7a16195707c6
alexandre.gabrielli <alexandre.gabrielli@users.noreply.github.com>
2019-05-10 15:34:10 +0200

    Rename: fonction add...() should start by lower case

--- Refactoring/src/test/java/ch/heigvd/gen2019/OrdersWriterTest.java
+++ Refactoring/src/test/java/ch/heigvd/gen2019/OrdersWriterTest.java
@@ -10,7 +10,7 @@ public class OrdersWriterTest {
 
     @BeforeEach
     public void SetupOneOrder() {
-        orders.AddOrder(order111);
+        orders.addOrder(order111);
     }
 
     @Test
@@ -26,7 +26,7 @@ public class OrdersWriterTest {
 
     @Test
     public void TwoOrders() {
-        orders.AddOrder(new Order(222));
+        orders.addOrder(new Order(222));
 
         String order111Json = JsonOrder111WithProduct("");
         String order222Json = "{\"id\": 222, \"products\": []}";
@@ -35,7 +35,7 @@ public class OrdersWriterTest {
 
     @Test
     public void OneOrderWithOneProduct() {
-        order111.AddProduct(new Product("Shirt", Color.BLUE, Size.M, 2.99, "TWD"));
+        order111.addProduct(new Product("Shirt", Color.BLUE, Size.M, 2.99, "TWD"));
 
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Shirt\", \"color\": \"blue\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
@@ -43,7 +43,7 @@ public class OrdersWriterTest {
 
     @Test
     public void OneOrderWithOneProductNoSize() {
-        order111.AddProduct(new Product("Pot", Color.RED, Size.SIZE_NOT_APPLICABLE, 16.50, "SGD"));
+        order111.addProduct(new Product("Pot", Color.RED, Size.SIZE_NOT_APPLICABLE, 16.50, "SGD"));
 
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
@@ -51,7 +51,7 @@ public class OrdersWriterTest {
 
     @Test
     public void OneOrderWithOneProducitNoColor(){
-        order111.AddProduct(new Product("Pot", Color.NO_COLOR, Size.SIZE_NOT_APPLICABLE, 16.50, "SGD"));
+        order111.addProduct(new Product("Pot", Color.NO_COLOR, Size.SIZE_NOT_APPLICABLE, 16.50, "SGD"));
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Pot\", \"color\": \"no color\", \"price\": 16.5, \"currency\": \"SGD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
 
