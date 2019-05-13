4a9e1c0c5e7599bad58d4a123d1b7f0fb1e5a3aa
alexandre.gabrielli <alexandre.gabrielli@users.noreply.github.com>
2019-05-13 14:21:23 +0200

    Pull Up Method In enum Size we can use the default implementation of toString because object name enum = stringification of this object

--- Refactoring/src/main/java/ch/heigvd/gen2019/Size.java
+++ Refactoring/src/main/java/ch/heigvd/gen2019/Size.java
@@ -1,48 +1,12 @@
 package ch.heigvd.gen2019;
 
 public enum Size {
-    XS {
-        @Override
-        public String toString() {
-            return "XS";
-        }
-    },
-    S {
-        @Override
-        public String toString() {
-            return "S";
-        }
-    },
-    M {
-        @Override
-        public String toString() {
-            return "M";
-        }
-    },
-    L {
-        @Override
-        public String toString() {
-            return "L";
-        }
-    },
-    XL {
-        @Override
-        public String toString() {
-            return "XL";
-        }
-    },
-    XXL {
-        @Override
-        public String toString() {
-            return "XXL";
-        }
-    },
-    SIZE_NOT_APPLICABLE {
-        @Override
-        public String toString() {
-            return "Invalid Size";
-        }
-    };
+    XS,
+    S,
+    M,
+    L,
+    XL,
+    XXL,
+    SIZE_NOT_APPLICABLE
 
-    public abstract String toString();
 }
